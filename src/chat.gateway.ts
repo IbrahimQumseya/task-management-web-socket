73a05a04cf844727590893a3bd0171569d80b844
IbrahimQumseya <52652965+IbrahimQumseya@users.noreply.github.com>
2022-03-07 16:02:13 +0200

    rooms

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,24 +1,40 @@
-import { Logger } from '@nestjs/common';
 import {
-  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
+  OnGatewayInit,
   WebSocketServer,
+  OnGatewayConnection,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({ namespace: '/chat' })
 export class ChatGateway implements OnGatewayInit {
-  private logger: Logger = new Logger('chatGateway');
-
   @WebSocketServer() wss: Server;
 
+  private logger: Logger = new Logger('ChatGateway');
+
   afterInit(server: any) {
-    this.logger.log('starting');
+    this.logger.log('Initialized!');
   }
 
   @SubscribeMessage('chatToServer')
-  handleMessage(client: Socket, message: { sender: string; message: string }) {
-    this.wss.emit('chatToClient', message);
+  handleMessage(
+    client: Socket,
+    message: { sender: string; room: string; message: string },
+  ) {
+    this.wss.to(message.room).emit('chatToClient', message);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleRoomJoin(client: Socket, room: string) {
+    client.join(room);
+    client.emit('joinedRoom', room);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleRoomLeave(client: Socket, room: string) {
+    client.leave(room);
+    client.emit('leftRoom', room);
   }
 }
